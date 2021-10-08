ae72f98cbc2315c2611f9bc2236f2970d834e907
Bhavyatha <bhavyathashetty@gmail.com>
2021-10-08 12:41:08 +0530

    adding files

---
+++ connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+var mysqlConnection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"newdb",
+    multipleStatements:true
+});
+mysqlConnection.connect((err)=>{
+    if(!err){
+        console.log("Connected");
+    }
+    else{
+        console.log("connection failed");
+    }
+});
+
+module.exports = mysqlConnection;
